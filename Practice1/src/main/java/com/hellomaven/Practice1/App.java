ca62f5ff4f620dc81c6b0ca872f47ec4ace391af
songbupt <ssong21@sina.com>
2023-10-26 09:36:13 +0800

    the 1st commit

---
+++ Practice1/src/main/java/com/hellomaven/Practice1/App.java
@@ -0,0 +1,13 @@
+package com.hellomaven.Practice1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello maven World!" );
+    }
+}
